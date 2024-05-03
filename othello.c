8afe512e385e627ba82219034c241a0ce063e23f
yuto-apf <yutochin.dev@gmail.com>
2024-05-04 01:02:25 +0900

    user input

--- othello.c
+++ othello.c
@@ -27,7 +27,9 @@ typedef char stone;
 
 void init_board(void);
 void print_board(void);
-void coord_cpy(coord *, coord *);
+coord *input_coord(coord *);
+void coord_copy(coord *, coord *);
+char to_upper(char);
 int is_in_board(coord *);
 int is_empty_square(coord *);
 int calc_distance(coord *, coord *);
@@ -48,6 +50,10 @@ int main(void) {
     init_board();
 
     test();
+    coord c;
+    while (input_coord(&c) != NULL) {
+        printf("%d, %d\n", c.x, c.y);
+    }
 
     return 0;
 }
@@ -83,9 +89,30 @@ void print_board(void) {
     }
 }
 
-void coord_cpy(coord *dst, coord *src) {
+coord *input_coord(coord *dst) {
+    char s[4];
+
+    if (fgets(s, 4, stdin) == NULL) {
+        return NULL;
+    }
+    s[1] = to_upper(s[1]);
+
+    if (('1' <= s[0] && s[0] <= '8') && ('A' <= s[1] && s[1] <= 'H')) {
+        dst->x = s[1] - 'A';
+        dst->y = s[0] - '0' - 1;
+        return dst;
+    } else {
+        return NULL;
+    }
+}
+
+void coord_copy(coord *dst, coord *src) {
     dst->x = src->x;
     dst->y = src->y;
+}
+
+char to_upper(char c) {
+    return ('a' <= c && c <= 'z') ? c - ('a' - 'A') : c;
 }
 
 int is_empty_square(coord *pos) {
@@ -156,7 +183,7 @@ int get_y_incremental_by_direction(direction dir) {
 coord *get_pair_coord(coord *dst, coord *pos, direction dir, stone color) {
     int x_inc, y_inc, cnt;
 
-    coord_cpy(dst, pos);
+    coord_copy(dst, pos);
     x_inc = get_x_incremental_by_direction(dir);
     y_inc = get_y_incremental_by_direction(dir);
     cnt = 0;
@@ -192,7 +219,7 @@ void flip_stone(coord *from, coord *to) {
     coord current;
     stone color;
 
-    coord_cpy(&current, from);
+    coord_copy(&current, from);
     dis = calc_distance(from, to);
     dir = calc_direction(from, to);
     x_inc = get_x_incremental_by_direction(dir);
@@ -207,28 +234,22 @@ void flip_stone(coord *from, coord *to) {
 }
 
 void test(void) {
-    int i;
     direction dir;
-    coord pos[] = {
-        {3, 2},
-        {2, 4},
-        {5, 5},
-        {3, 1},
-        {0, 0}
-    };
-    coord pair;
+    coord pos, pair;
     stone color = BLACK;
 
     print_board();
-    for (i = 0; i < sizeof(pos) / sizeof(pos[0]); i++) {
+    while (1) {
         printf("\n");
-        printf("*** put '%c' on %d%c\n\n", color, pos[i].y + 1, "ABCDEFGH"[pos[i].x]);
+        do {
+            printf("Where do you place the stone '%c' ?\n", color);
+            printf(" >> ");
+        } while (input_coord(&pos) == NULL);
 
         for (dir = TOP_LEFT; dir <= LEFT; dir++) {
-            if (get_pair_coord(&pair, &pos[i], dir, color) != NULL) {
-                put_stone(&pos[i], color);
-                flip_stone(&pos[i], &pair);
-                printf("found a pair towards '%d'\n", dir);
+            if (get_pair_coord(&pair, &pos, dir, color) != NULL) {
+                put_stone(&pos, color);
+                flip_stone(&pos, &pair);
             }
         }
         printf("\n");
